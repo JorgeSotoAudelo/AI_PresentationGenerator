b1597f765fe1d9324f58bac194db9de6ef306f6f
JorgeSotoAudelo <115675512+JorgeSotoAudelo@users.noreply.github.com>
2023-06-12 18:53:06 +0000

    Creando la pag principal

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
-from flask import Flask
+""""The main app of my flask proyect"""
 
-app = Flask('app')
+
+from flask import Flask, render_template
+app = Flask('run',template_folder='app/views')
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
